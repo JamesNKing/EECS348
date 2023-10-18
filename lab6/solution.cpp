78c584daa86e383b9176c54b5aa00c050c515d41
JamesNKing <jamieking2004@gmail.com>
2023-10-18 14:25:38 -0500

    started lab6 with objectives and added input file

---
+++ lab6/solution.cpp
@@ -0,0 +1,24 @@
+/* performs operations square matrices using funcs and arrays
+ * - read matrix data from a file
+ * - perform matrix operations
+ * - output the results
+ *
+ * - max size of square matrices defined as static const 100 but actual size
+ *   of square matrix will be the first line of data, being <= 100. 
+ *
+ * functions:
+ * 1. read values from a file into a matrix
+ * - store in 2D array
+ *
+ * 2. print a matrix
+ * - values should nicely allign in a column
+ *
+ * 3. add two matrices and place the result in a third matrix; print result 
+ *
+ * 4. multiply two matrices and place result in a matrix, print result
+ *
+ * 5. subtract second matrix from the first and place result in third, print
+ *
+ * constraints: 
+ * - NxN matrices, N being the static constant. Input from 'matrix_input.txt'
+ * - place function prototypes before main, write their bodies in main
